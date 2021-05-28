5026a6b2357d4f51cd335f49fab576cc0f74bd65
sgk-switch <80930480+sgk-switch@users.noreply.github.com>
2021-05-28 19:23:26 +0900

    相対パスへの変更

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,11 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  /*
   publicPath: process.env.NODE_ENV === 'production'
     ? '/todolist-app/'   //任意
     : '/'
+  */
+  publicPath: './'
+  
 }
