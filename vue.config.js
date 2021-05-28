e4d17a5a365ca01d3e94b0a961c503203f7819eb
sgk-switch <80930480+sgk-switch@users.noreply.github.com>
2021-05-28 19:34:28 +0900

    パスを元に戻す

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,8 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  /*
   publicPath: process.env.NODE_ENV === 'production'
     ? '/todolist-app/'   //任意
     : '/'
-  */
-  publicPath: './'
-  
+
 }
