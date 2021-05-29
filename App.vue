915e1d8484c793f6ee099fac203200337b14867d
sgk-switch <80930480+sgk-switch@users.noreply.github.com>
2021-05-29 15:30:18 +0900

    Todo新規作成モーダルの閉じるの実装とTodoEdit.vueの追加

---
+++ App.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+       <v-toolbar-title>My TodoList</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn 
+        text
+        @click="openModal()"
+      >
+        新規作成
+      </v-btn>
+
+    </v-app-bar>
+
+    <v-main>
+      <Board
+        :isShowModal='isShowModal' 
+        @custom-close="changeIsShow()"
+      />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Board from './components/Board.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Board,
+  },
+
+  data(){
+    return{
+      isShowModal: false,
+    }
+  },
+
+  methods:{
+    openModal(){
+      this.isShowModal = !this.isShowModal
+      console.log(this.isShowModal)
+    },
+    changeIsShow(){
+      this.isShowModal = false
+    }
+  }
+
+};
+</script>
